4cceecd5e5bb8588f953b48aa93456962d1033ab
Vladimir Ledovskikh <vladimir.ledovskih@red-soft.ru>
2026-06-10 18:29:01 +0300

    Add -f / --format option

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,6 +5,8 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(prog="gendiff", description=DESCRIPTION)
 
+    parser.add_argument("-f", "--format", dest="FORMAT", help="set format of output")
+
     parser.add_argument("first_file")
     parser.add_argument("second_file")
 
