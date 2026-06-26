254402e27116fc8640ce0ecf5fc864e4552f3527
Vladimir Ledovskih <vladimir.ledovskih@red-soft.ru>
2026-06-27 01:57:00 +0300

    Add plain_json styler

--- gendiff/formatting/stylers.py
+++ gendiff/formatting/stylers.py
@@ -1,3 +1,4 @@
+import json
 def stylish(diff: list) -> str:
     result = "{\n"
     change_symbols = {
@@ -27,3 +28,20 @@ def plain(diff: list) -> str:
         elif change_type == "new_value":
             result += f"{value}\n"
     return result.strip()
+
+
+def plain_json(diff: list) -> str:
+    result = {}
+    for item in diff:
+        change_type, key, value = item.split(":", 2)
+        if value.lower() == "true":
+            value = True
+        elif value.lower() == "false":
+            value = False
+        if change_type in ["no_key", "new_key", "same"]:
+            result[key] = {"change": change_type, "value": value}
+        elif change_type == "old_value":
+            result[key] = {"change": change_type, "old_value": value}
+        elif change_type == "new_value":
+            result[key]["new_value"] = value
+    return json.dumps(result)
