9bec72cbee6184b9c61b0bb8a12a2b0d3e42865d
Vladimir Ledovskikh <vladimir.ledovskih@red-soft.ru>
2026-06-26 17:35:48 +0300

    Add generate_diff and return_str_key

--- gendiff/general/file_helper.py
+++ gendiff/general/file_helper.py
@@ -14,4 +14,38 @@ def open_txt(path: str) -> str:
 def open_file(path: str) -> dict | str:
     if path.endswith(".json"):
         return open_json(path)
-    return open_txt(path)
+    if path.endswith(".txt"):
+        return open_txt(path)
+    return ""
+
+
+def return_str_key(json: dict, key: str) -> str:
+    value = json.get(key, False)
+    if isinstance(value, bool):
+        return str(value).lower()
+    return value
+
+
+def generate_diff(path_1: str, path_2: str) -> str:
+    file_1 = open_file(path_1)
+    file_2 = open_file(path_2)
+    if isinstance(file_1, dict) and isinstance(file_2, dict):
+        result_json = {**file_1, **file_2}
+        sorted_data = {
+            key: return_str_key(result_json, key) for key in sorted(result_json.keys())
+        }
+        result = "{\n"
+        for key in sorted_data.keys():
+            if key not in file_2:
+                result += f"  - {key}: {sorted_data[key]}\n"
+            elif key not in file_1:
+                result += f"  + {key}: {sorted_data[key]}\n"
+            else:
+                if sorted_data[key] == file_1[key]:
+                    result += f"    {key}: {sorted_data[key]}\n"
+                else:
+                    result += f"  - {key}: {sorted_data[key]}\n"
+                    result += f"  + {key}: {file_1[key]}\n"
+        result += "}"
+        return result
+    return ""
