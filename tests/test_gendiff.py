3ec4e12e1d04100ab15cd16eca0fb515ac7d9829
Vladimir Ledovskih <vladimir.ledovskih@red-soft.ru>
2026-06-26 23:30:46 +0300

    Add tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,11 +7,29 @@ TEST_DATA_DIR = os.path.join(
 )
 
 
-def test_generate_diff():
+def test_generate_diff_general():
     assert (
         generate_diff(
             os.path.join(TEST_DATA_DIR, "file1.json"),
             os.path.join(TEST_DATA_DIR, "file2.json"),
         )
-        == open(os.path.join(TEST_DATA_DIR, "test_1_result.txt")).read()
+        == open(os.path.join(TEST_DATA_DIR, "result_test_1.txt")).read()
+    )
+
+def test_generate_diff_one_line():
+    assert (
+        generate_diff(
+            os.path.join(TEST_DATA_DIR, "file_one_line_1.json"),
+            os.path.join(TEST_DATA_DIR, "file_one_line_2.json"),
+        )
+        == open(os.path.join(TEST_DATA_DIR, "result_test_2.txt")).read()
+    )
+
+def test_generate_diff_empty():
+    assert (
+        generate_diff(
+            os.path.join(TEST_DATA_DIR, "file_empty_1.json"),
+            os.path.join(TEST_DATA_DIR, "file_empty_2.json"),
+        )
+        == open(os.path.join(TEST_DATA_DIR, "result_test_3.txt")).read()
     )
